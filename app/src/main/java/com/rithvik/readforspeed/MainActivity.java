241ec7791ab1cd858f7208284ffdb13f17870f6f
Rithvik <vikturi99@gmail.com>
2018-05-29 15:06:47 -0500

    Added code to make single words appear at timed intervals

--- app/src/main/java/com/rithvik/readforspeed/MainActivity.java
+++ app/src/main/java/com/rithvik/readforspeed/MainActivity.java
@@ -1,5 +1,6 @@
 package com.rithvik.readforspeed;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +12,9 @@ import java.util.concurrent.TimeUnit;
 public class MainActivity extends AppCompatActivity {
     Button b1;
     EditText e1;
+    private int interval = 5000;
+    private Handler h;
+    String[] gArr;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,21 +22,45 @@ public class MainActivity extends AppCompatActivity {
 
         b1 = (Button) findViewById(R.id.button);
         e1 = (EditText) findViewById(R.id.editText);
+        h = new Handler();
 
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
                 String given = e1.getText().toString();
-                String[] gArr = given.split(" ");
-                for (String g : gArr) {
-                    try {
-                        e1.setText(g);
-                        TimeUnit.SECONDS.sleep(10);
-                    } catch(InterruptedException ex) {
-                        Thread.currentThread().interrupt();
-                    }
-                }
+                gArr = given.split(" ");
+                startRepeatingTask();
             }
         });
+    }
+    int index = 0;
+
+    Runnable sc = new Runnable() {
+        @Override
+        public void run() {
+            try {
+                if (index < gArr.length) {
+                    e1.setText(gArr[index]);
+                    index++;
+                }
+            }
+            finally {
+                h.postDelayed(sc, interval);
+            }
+        }
+    };
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        stopRepeatingTask();
+    }
+
+    void startRepeatingTask() {
+        sc.run();
+    }
+
+    void stopRepeatingTask() {
+        h.removeCallbacks(sc);
     }
 }
