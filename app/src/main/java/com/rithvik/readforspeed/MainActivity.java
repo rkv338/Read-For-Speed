5fdfeaafe06731f89ad06d3ae3946d6bf6c41221
Rithvik <vikturi99@gmail.com>
2018-05-29 14:09:25 -0500

    Created text field to type in and button

--- app/src/main/java/com/rithvik/readforspeed/MainActivity.java
+++ app/src/main/java/com/rithvik/readforspeed/MainActivity.java
@@ -2,12 +2,37 @@ package com.rithvik.readforspeed;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button b1;
+    EditText e1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b1 = (Button) findViewById(R.id.button);
+        e1 = (EditText) findViewById(R.id.editText);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                String given = e1.getText().toString();
+                String[] gArr = given.split(" ");
+                for (String g : gArr) {
+                    try {
+                        e1.setText(g);
+                        TimeUnit.SECONDS.sleep(10);
+                    } catch(InterruptedException ex) {
+                        Thread.currentThread().interrupt();
+                    }
+                }
+            }
+        });
     }
 }
