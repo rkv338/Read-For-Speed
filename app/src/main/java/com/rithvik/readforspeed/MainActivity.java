33e109b9bf369134694c3c59d25a32e783ebb70c
Rithvik <vikturi99@gmail.com>
2018-05-30 16:00:28 -0500

    Added main screen and a reading activity
    Clicking the button makes the app go to the reading activity. Different speeds also implemented on the reading activity

--- app/src/main/java/com/rithvik/readforspeed/MainActivity.java
+++ app/src/main/java/com/rithvik/readforspeed/MainActivity.java
@@ -1,5 +1,7 @@
 package com.rithvik.readforspeed;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,56 +13,25 @@ import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
     Button b1;
-    EditText e1;
-    private int interval = 5000;
-    private Handler h;
-    String[] gArr;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         b1 = (Button) findViewById(R.id.button);
-        e1 = (EditText) findViewById(R.id.editText);
-        h = new Handler();
 
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
-                String given = e1.getText().toString();
-                gArr = given.split(" ");
-                startRepeatingTask();
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Reading.class));
             }
         });
     }
-    int index = 0;
 
-    Runnable sc = new Runnable() {
-        @Override
-        public void run() {
-            try {
-                if (index < gArr.length) {
-                    e1.setText(gArr[index]);
-                    index++;
-                }
-            }
-            finally {
-                h.postDelayed(sc, interval);
-            }
-        }
-    };
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        stopRepeatingTask();
-    }
 
-    void startRepeatingTask() {
-        sc.run();
-    }
 
-    void stopRepeatingTask() {
-        h.removeCallbacks(sc);
-    }
+
+
+
 }
---
+++ app/src/main/java/com/rithvik/readforspeed/Reading.java
@@ -0,0 +1,90 @@
+package com.rithvik.readforspeed;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+public class Reading extends AppCompatActivity {
+
+    Button b2;
+    EditText e2;
+    private int interval = 150;
+    private Handler h;
+    String[] gArr;
+    Spinner speedChoice;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reading);
+        h = new Handler();
+        b2 = (Button) findViewById(R.id.button2);
+        e2 = (EditText) findViewById(R.id.editText2);
+        speedChoice = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter =  new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.speed));
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        speedChoice.setAdapter(adapter);
+
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                String given = e2.getText().toString();
+                gArr = given.split(" ");
+                int speedPos = speedChoice.getSelectedItemPosition();
+                double speed = giveSpeed(speedPos);
+                interval = (int) ((60 / speed) * 1000);
+                startRepeatingTask();
+            }
+        });
+    }
+    int index = 0;
+
+    Runnable sc = new Runnable() {
+        @Override
+        public void run() {
+            try {
+                if (index < gArr.length) {
+                    e2.setText(gArr[index]);
+                    index++;
+                }
+                else {
+                    finish();
+                }
+            }
+            finally {
+                h.postDelayed(sc, interval);
+            }
+        }
+    };
+
+    @Override
+    public void onDestroy() {
+
+        super.onDestroy();
+        stopRepeatingTask();
+    }
+
+    void startRepeatingTask() {
+        sc.run();
+    }
+
+    void stopRepeatingTask() {
+        h.removeCallbacks(sc);
+    }
+
+    double giveSpeed (int index) {
+        switch (index) {
+            case 1: return 400.0;
+            case 2: return 500.0;
+            case 3: return 600.0;
+            case 4: return 700.0;
+        }
+        return index * 1.0;
+    }
+}
